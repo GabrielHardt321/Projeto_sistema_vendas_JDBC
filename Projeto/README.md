0f579e8c9c88c4a9108d264e61a41c96527d82d2
Gabriel <gabrielhardtklipegokusuper@gmail.com>
2026-05-06 20:36:54 -0300

    Estrutura inicial do Mini ERP com JDBC

---
+++ Projeto/README.md
@@ -0,0 +1,7 @@
+# Mini ERP - Java JDBC
+Sistema de gestão de vendas desenvolvido para estudo de Java + MySQL.
+
+## Tecnologias
+- Java
+- MySQL (JDBC)
+- VS Code
---
+++ Projeto/src/conexao/Conexao.java
@@ -0,0 +1,20 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_vendas";
+    private static final String USER = "root";
+    private static final String PASS = "Sua_senha";
+
+    public static Connection getConexao() {
+        try{
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+            return null;
+        }
+    }
+}
