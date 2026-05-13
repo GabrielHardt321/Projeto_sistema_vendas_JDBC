eb1ec9c2d3a7212216151dcbb232640b47c3239d
engs-JoaoOliveira <engs-joaooliveira@camporeal.edu.br>
2026-05-12 21:05:08 -0300

    Criação da classe Clientes e inicio da criação do DAO

---
+++ Projeto/src/entidades/Clientes.java
@@ -0,0 +1,20 @@
+
+public class Clientes {
+    
+    private int id_clientes;
+    private String nome_clientes;
+    private String CPF;
+
+    public String getNome() {
+        return nome_clientes;
+    }
+
+    public int getId() {
+        return id_clientes;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+}
